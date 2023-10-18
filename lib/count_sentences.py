af00adf5d0aa367ce8234b3a5ed3a3b52c028d8d
Kelvin okumba <okumbakelvin@gmail.com>
2023-10-18 14:10:19 +0300

    completed

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
+import re
+
 class MyString:
-  pass
+    def __init__(self):
+        self.value = ''
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        if not isinstance(val, str):
+            raise ValueError("Value must be a string")
+        self._value = val
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        # Split the value using punctuation characters as separators
+        sentences = re.split(r'[.!?]', self.value)
+        # Remove empty strings from the list
+        sentences = [s.strip() for s in sentences if s.strip()]
+        return len(sentences)
+
